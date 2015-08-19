cf9a3f6beecd10a748f2f1d0e7dda4be2e2fd839
dafap <dafap@free.fr>
2015-08-19 22:36:36 +0200

    Modification de la structure d'appel des documents pdf tabulaires. Utilisation d'une surcharge de Select pour passer les critères de filtrage. Fin de la gestion des affectations des documents dans les menus d'impression.

--- module/SbmPdf/config/module.config.php
+++ module/SbmPdf/config/module.config.php
@@ -10,32 +10,37 @@
  * @date 1 juil. 2015
  * @version 2015-2
  */
-
 return array(
     'tcpdf' => array(),
     'acl' => array(
         'resources' => array(
             'sbmpdf' => array(
                 'allow' => array(
-                    'roles' => array('admin', 'sadmin')
+                    'roles' => array(
+                        'admin',
+                        'sadmin'
+                    )
                 )
             )
-        ),
+        )
     ),
     'liste' => array(
         'paginator' => array(
-            'nb_pdf' => 1,
+            'nb_pdf' => 1
         )
     ),
     'service_manager' => array(
         'invokables' => array(
             'RenderPdfService' => 'SbmPdf\Service\RenderPdfService',
             'PdfListener' => 'SbmPdf\Listener\PdfListener'
+        ),
+        'factories' => array(
+            'ListeRoutes' => 'SbmPdf\Service\ListeRoutesService'
         )
     ),
     'controllers' => array(
         'invokables' => array(
-            'SbmPdf\Controller\Document' => 'SbmPdf\Controller\DocumentController',
+            'SbmPdf\Controller\Document' => 'SbmPdf\Controller\DocumentController'
         )
     ),
     'router' => array(
@@ -43,10 +48,11 @@ return array(
             'sbmpdf' => array(
                 'type' => 'segment',
                 'options' => array(
-                    'route' => '/pdf[/:action[/:page]]',
+                    'route' => '/pdf[/:action[/page/:page][/id/:id]]',
                     'constraints' => array(
                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'page' => '[0-9]+'
+                        'page' => '[0-9]+',
+                        'id' => '[0-9]+'
                     ),
                     'defaults' => array(
                         'module' => 'SbmPdf',
