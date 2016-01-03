24a4763fa5546b87fcb933eb53c3baeb2ea1d753
dafap <dafap@free.fr>
2016-01-03 23:03:09 +0100

    Amélioration : gestion d'une exception.

--- module/SbmGestion/src/SbmGestion/Controller/TransportController.php
+++ module/SbmGestion/src/SbmGestion/Controller/TransportController.php
@@ -104,8 +104,11 @@ class TransportController extends AbstractActionController
     {
         $currentPage = $this->params('page', 1);
         $form = new FormCircuit();
-        $form->setValueOptions('serviceId', $this->getServiceLocator()->get('Sbm\Db\Select\Services'))
-            ->setValueOptions('stationId', $this->getServiceLocator()->get('Sbm\Db\Select\Stations')->ouvertes())
+        $form->setValueOptions('serviceId', $this->getServiceLocator()
+            ->get('Sbm\Db\Select\Services'))
+            ->setValueOptions('stationId', $this->getServiceLocator()
+            ->get('Sbm\Db\Select\Stations')
+            ->ouvertes())
             ->setValueOptions('semaine', Semaine::getJours());
         $params = array(
             'data' => array(
@@ -226,7 +229,16 @@ class TransportController extends AbstractActionController
             ),
             'form' => $form
         );
-        $r = $this->addData($params);
+        try {
+            $r = $this->addData($params);
+        } catch (\Zend\Db\Adapter\Exception\InvalidQueryException $e) {
+            if (stripos($e->getMessage(), '23000 - 1062 - Duplicate entry') !== false) {
+                $this->flashMessenger()->addWarningMessage('Impossible ! Cet arrêt est déjà sur ce circuit.');
+                $r = 'warning';
+            } else {
+                throw new \Zend\Db\Adapter\Exception\InvalidQueryException($e->getMessage(), $e->getCode(), $e->getPrevious());
+            }
+        }
         switch ($r) {
             case $r instanceof Response:
                 return $r;
@@ -685,12 +697,12 @@ class TransportController extends AbstractActionController
      */
     public function communeListeAction()
     {
-        //die(var_dump($this->getFromSession('post', 'vide', $this->getSessionNamespace())));
+        // die(var_dump($this->getFromSession('post', 'vide', $this->getSessionNamespace())));
         $args = $this->initListe('communes');
         
         if ($args instanceof Response)
             return $args;
-        //die(var_dump($args['form']));
+            // die(var_dump($args['form']));
         return new ViewModel(array(
             'paginator' => $this->getServiceLocator()
                 ->get('Sbm\Db\Table\Communes')
@@ -2322,12 +2334,11 @@ class TransportController extends AbstractActionController
 
     /**
      * Ajout d'une nouvelle fiche de station
-     * 
+     *
      * ANCIENNE VERSION
      * (la validation porte sur un champ csrf)
      *
-     * @return \Zend\View\Model\ViewModel 
-     * public function stationAjoutAction()
+     * @return \Zend\View\Model\ViewModel public function stationAjoutAction()
      *         {
      *         $currentPage = $this->params('page', 1);
      *         $form = new FormStation();
@@ -2366,16 +2377,16 @@ class TransportController extends AbstractActionController
      *         }
      */
     
-     /**
-      * Montre la carte des stations.
-      * Un clic dans la carte permet de placer la nouvelle station. 
-      * On enregistre la position.
-      * Le formulaire prérempli est présenté avec la commune, l'adresse (N° + rue) 
-      * et les coordonnées X et Y
-      * On peut alors changer le nom de la station avant d'enregistrer la fiche.
-      * 
-      * @return \Zend\Http\PhpEnvironment\Response|\Zend\Http\Response|\Zend\View\Model\ViewModel
-      */
+    /**
+     * Montre la carte des stations.
+     * Un clic dans la carte permet de placer la nouvelle station.
+     * On enregistre la position.
+     * Le formulaire prérempli est présenté avec la commune, l'adresse (N° + rue)
+     * et les coordonnées X et Y
+     * On peut alors changer le nom de la station avant d'enregistrer la fiche.
+     *
+     * @return \Zend\Http\PhpEnvironment\Response|\Zend\Http\Response|\Zend\View\Model\ViewModel
+     */
     public function stationAjoutAction()
     {
         $currentPage = $this->params('page', 1);
@@ -2738,7 +2749,8 @@ class TransportController extends AbstractActionController
 
     /**
      * Localisation d'une station sur la carte et enregistrement de ses coordonnées
-     * Toutes les stations sont affichées. 
+     * Toutes les stations sont affichées.
+     *
      * La station à localiser est repérée par un bulet rouge.
      */
     public function stationLocalisationAction()
