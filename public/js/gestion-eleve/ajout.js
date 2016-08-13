714ae1d5e08ac489f41093267937d4dd52e9ad84
dafap <dafap@free.fr>
2016-08-13 22:26:42 +0200

    Refactoring du code javascript et jquery.

---
+++ public/js/gestion-eleve/ajout.js
@@ -0,0 +1,27 @@
+/**
+ * scripts dela page de sbm-gestion/eleve/eleve-ajout.phtml
+ * 
+ * @project sbm
+ * @filesource gestion-eleve.js
+ * @encodage UTF-8
+ * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
+ * @date 13 août 2016
+ * @version 2016-2.1.10
+ */
+
+var js_ajout = (function() {
+	$(document).ready(function($) {
+		$("#eleve-ga").click(function() {
+			js_ajout.montreResponsable2($(this).is(":checked"));
+		});
+	});
+	return {
+		"montreResponsable2" : function(voir) {
+			if (voir) {
+				$("#wrapper-responsable2Id").show();
+			} else {
+				$("#wrapper-responsable2Id").hide();
+			}
+		}
+	};
+})();
---
+++ public/js/gestion-eleve/edit.js
@@ -0,0 +1,364 @@
+/**
+ * Ensemble des scripts des pages de sbm-gestion/eleve/eleve-edit.phtml
+ * 
+ * @project sbm
+ * @filesource gestion-eleve.js
+ * @encodage UTF-8
+ * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
+ * @date 13 août 2016
+ * @version 2016-2.1.10
+ */
+
+var js_edit = (function(){
+	function initAccord(disableAccordR1, disableAccordR2) {
+		if (disableAccordR1) {
+			$("#eleve-accordR1").attr("disabled", true);
+			$("input[name=accordR1][type=hidden]").val(1);
+		} else {
+			$("#eleve-accordR1").removeAttr("disabled");
+			$("input[name=accordR1][type=hidden]").val(0);
+		}
+		;
+		if (disableAccordR2) {
+			$("#eleve-accordR2").attr("disabled", true);
+			$("input[name=accordR2][type=hidden]").val(1);
+		} else {
+			$("#eleve-accordR2").removeAttr("disabled");
+			$("input[name=accordR2][type=hidden]").val(0);
+		}
+	}
+	function montreDebutFin(ancomplet) {
+		if (ancomplet) {
+			$("#wrapper-dateDebut").hide();
+			$("#wrapper-dateFin").hide();
+		} else {
+			$("#wrapper-dateDebut").show();
+			$("#wrapper-dateFin").show();
+		}
+	}
+	function montreMotifDerogation(derogation) {
+		if (derogation) {
+			$("#wrapper-motifDerogation").show();
+		} else {
+			$("#wrapper-motifDerogation").hide();
+		}
+	}
+	function montreOngletGa(voir) {
+		if (voir) {
+			$("#tabs li").eq(1).show();
+		} else {
+			$("#tabs li").eq(1).hide();
+		}
+	}
+	// pour les fonctions suivantes r prend la valeur 'r1' ou 'r2'
+	function montreDemande(r) {
+		if ($("#demande" + r + "radio0").is(":checked")) {
+			$("#block-demande" + r).hide();
+			$("#block-affectations" + r).hide();
+		} else {
+			$("#block-demande" + r).show();
+			$("#block-affectations" + r).show();
+		}
+	}
+	function montreMotifRefus(accord, subvention, r) {
+		if (accord) {
+			if (subvention) {
+				$("#wrapper-motifRefus" + r).show();
+			} else {
+				$("#wrapper-motifRefus" + r).hide();
+			}
+		} else {
+			$("#wrapper-motifRefus" + r).show();
+		}
+	}
+	function montreBlockAffectation(accord, r) {
+		if (accord) {
+			$("#block-affectations" + r).show();
+		} else {
+			$("#block-affectations" + r).hide();
+		}
+	}
+	function montreResponsable(r, data) {
+		var oresponsable = $.parseJSON(data);
+		var part_html;
+		var responsable;
+		responsable = oresponsable.titre + ' ' + oresponsable.nom + ' '
+				+ oresponsable.prenom;
+		$("#" + r + "-ligne1").html(responsable);
+		$("#" + r + "-ligne2").html(
+				oresponsable.adresseL1 + ' ' + oresponsable.adresseL2);
+		$("#" + r + "-ligne3").html(
+				oresponsable.codePostal + ' ' + oresponsable.commune);
+		if (!!oresponsable.email) {
+			part_html = [];
+			part_html.push('email: ' + oresponsable.email);
+			part_html.push('<div id="email' + r
+					+ '" style="display: inline; margin-left: 5px;">');
+			part_html.push('<input type="hidden" name="email' + r + '" value="'
+					+ oresponsable.email + '">');
+			part_html.push('<input type="hidden" name="responsable' + r
+					+ '" value="' + responsable + '">');
+			part_html.push('<input type="hidden" name="group" value="'
+					+ URL_ICI + '">');
+			part_html
+					.push('<input type="submit" name="ecrire'
+							+ r
+							+ '" class="fam-email" title="Envoyer un email" value="" formaction="/gestion/eleve/responsable-mail">');
+			part_html.push('</div>');
+			$("#" + r + "-ligne4").html(part_html.join('\n'));
+		} else {
+			$("#" + r + "-ligne4").empty();
+		}
+		part_html = [ 'Tél. ' ];
+		if (!!oresponsable.telephoneF) {
+			part_html.push(oresponsable.telephoneF);
+		}
+		if (!!oresponsable.telephoneP) {
+			part_html.push(oresponsable.telephoneP);
+		}
+		if (!!oresponsable.telephoneT) {
+			part_html.push(oresponsable.telephoneT);
+		}
+		$("#" + r + "-ligne5").html(part_html.join(' '));
+	}
+		
+	$(document).ready(function($) {
+		$("#duplicatamoins").click(function(){
+		    $.ajax({
+		        url:'/sbmajaxeleve/decrementeduplicata/eleveId:'+ELEVE_ID,
+			    dataType: 'json',
+		        success:function(data){
+		            var myid="#nbduplicata";
+		            var duplicata=data.duplicata;
+		            $(myid).empty();
+					$(myid).append(duplicata.toString());
+		        },
+		        error:function(xhr,ajaxOptions,thrownError){
+		            alert(xhr.status+' '+thrownError);
+		        }
+		    });
+		});
+		$("#eleve-anneeComplete").click(function(){
+		    montreDebutFin($(this).is(":checked"));
+		});
+		$("#eleve-derogation").click(function(){
+		    montreMotifDerogation($(this).is(":checked"));
+		});
+		$("#eleve-ga").click(function(){
+		    montreOngletGa($(this).is(":checked"));
+		});
+		$("#fiche-inner input[name=demandeR1]").click(function(){
+		    montreDemande('r1');
+		});
+		$("#fiche-inner input[name=demandeR2]").click(function(){
+		    montreDemande('r2');
+		});
+		$("#eleve-accordR1").click(function(){
+		    var action = ($(this).is(':checked'))?'check':'uncheck';
+		    var subvention = $("#eleve-subventionR1").is(':checked');
+		    $.ajax({
+		        url:'/sbmajaxeleve/'+action+'accordR1/eleveId:'+ELEVE_ID,
+		        success:function(data){
+		            montreMotifRefus(action=='check',subvention,'r1');
+		            montreBlockAffectation(action=='check','r1');
+		        },
+		        error:function(xhr,ajaxOptions,thrownError){
+		            alert(xhr.status+' '+thrownError);
+		        }
+		    });
+		});
+		$("#eleve-subventionR1").click(function(){
+		    var accord = $("#eleve-accordR1").is(':checked');
+		    var subvention = $(this).is(':checked');
+		    montreMotifRefus(accord,subvention,'r1');
+		});
+		$("#eleve-accordR2").click(function(){
+		    var action = ($(this).is(':checked'))?'check':'uncheck';
+		    var subvention = $("#eleve-subventionR2").is(':checked');
+		    $.ajax({
+		        url:'/sbmajaxeleve/'+action+'accordR2/eleveId:'+ELEVE_ID,
+		        success:function(data){
+		            montreMotifRefus(action=='check',subvention,'r2');
+		            montreBlockAffectation(action=='check','r2');
+		        },
+		        error:function(xhr,ajaxOptions,thrownError){
+		            alert(xhr.status+' '+thrownError);
+		        }
+		    });
+		});
+		$("#eleve-subventionR2").click(function(){
+		    var accord = $("#eleve-accordR2").is(':checked');
+		    var subvention = $(this).is(':checked');
+		    montreMotifRefus(accord,subvention,'r2');
+		});
+		$("input[type='text'][name^='distanceR']").dblclick(function(){
+		    $(this).css("cursor", "wait");
+		    var myid = '#'+$(this).attr('id');
+		    var name = $(this).attr('name');
+		    var n = 1;
+		    if (name.indexOf('1') == -1) {
+		        n = 2;
+		    }
+		    var id = '#eleve-responsable'+n+'Id';
+		    var responsableid = $(id).val();
+		    var etablissementid = $("#eleve-etablissementId").val();
+		    var args = 'etablissementId:'+etablissementid+'/responsableId:'+responsableid;
+		    $.ajax({
+					url : '/sbmajaxeleve/donnedistance/'+args,
+					type: 'GET',
+					dataType: 'json',
+					success : function(data){
+					    $(myid).val(data.distance);
+					    $(myid).css('cursor', 'auto');
+		            },
+					error : function(xhr, ajaxOptions, thrownError) {
+							alert(xhr.status + " " + thrownError);
+							$(myid).css('cursor','auto');
+					}
+			});
+		});
+		$("#eleve-responsable1Id").change(function(){
+		    var responsableid = $(this).val();
+		    $.ajax({
+					url : '/sbmajaxeleve/getresponsable/responsableId:'+responsableid,
+					success : function(data){montreResponsable('r1',data);},
+					error : function(xhr, ajaxOptions, thrownError) {
+							alert(xhr.status + " " + thrownError);
+					}
+			});
+		});
+		$("#eleve-responsable2Id").change(function(){
+		    var responsableid = $(this).val();
+		    $.ajax({
+					url : '/sbmajaxeleve/getresponsable/responsableId:'+responsableid,
+					success : function(data){montreResponsable('r2',data);},
+					error : function(xhr, ajaxOptions, thrownError) {
+							alert(xhr.status + " " + thrownError);
+					}
+			});
+		});
+		$("#tabs").on('click', "i[data-button=btnaffectation]", function(){
+		    var trajet = $(this).attr('data-trajet');
+		    var respid ='#eleve-responsable'+trajet+'Id';
+		    var href = '/sbmajaxeleve/formaffectation/eleveId:'+ELEVE_ID+'/trajet:'+trajet+$(this).attr('data-href')+'/responsableId:';
+		    href = href.concat($(respid).val());
+		    $("#winpopup").dialog({
+		            draggable:true,
+		            modal: true,
+		            autoOpen: false,
+		            height:400,
+		            width:600,
+		            resizable: false,
+		            title:$(this).attr('title')//,
+		            //position:'center'
+		        });
+		        $("#winpopup").load(href);
+		        $("#winpopup").dialog("open");
+		         
+		        return false;
+		});
+		$("#eleve-btnpaiement").click(function(){
+		    var href = '/sbmajaxeleve/formpaiement/eleveId:'+ELEVE_ID;
+		    $("#winpopup").dialog({
+		            draggable:true,
+		            modal:true,
+		            autoOpen:false,
+		            height:400,
+		            width:600,
+		            resizable:false,
+		            title:'Destinataire de la facture'    
+		    });
+		    $("#winpopup").load(href);
+		    $("#winpopup").dialog("open");
+		    
+		    return false;
+		});
+		});
+	return {
+		"init" : function(disableAccordR1, disableAccordR2){
+			$("#tabs").tabs();
+			montreDebutFin($("#eleve-anneeComplete").is(":checked"));
+			montreMotifDerogation($("#eleve-derogation").is(":checked"));
+			montreOngletGa($("#eleve-ga").is(":checked"));
+			montreMotifRefus($("#eleve-accordR1").is(":checked"),$("#eleve-subventionR1").is(":checked"),'r1');
+			montreMotifRefus($("#eleve-accordR2").is(":checked"),$("#eleve-subventionR2").is(":checked"),'r2');
+			montreBlockAffectation($("#eleve-accordR1").is(":checked"),'r1');
+			montreBlockAffectation($("#eleve-accordR2").is(":checked"),'r2');
+			initAccord(disableAccordR1, disableAccordR2);
+			$.ajax({
+					url:'/sbmajaxeleve/getresponsable/responsableId:'+$("#eleve-responsable1Id").val(),
+					success:function(data){montreResponsable('r1',data);},
+					error:function(xhr, ajaxOptions, thrownError) {
+						alert(xhr.status + " " + thrownError);
+					}
+			});
+			if ($("#eleve-responsable2Id").val()) {
+			    $.ajax({
+					url:'/sbmajaxeleve/getresponsable/responsableId:'+$("#eleve-responsable2Id").val(),
+					success:function(data){montreResponsable('r2',data);},
+					error:function(xhr, ajaxOptions, thrownError) {
+						alert(xhr.status + " " + thrownError);
+					}
+			    });
+			    montreDemande('r1');
+			    montreDemande('r2');
+			}
+		},
+		"majBlockAffectations":function(trajet) {
+			$('html', 'body').css('cursor', 'auto');
+			var args1 = 'eleveId:' + ELEVE_ID + '/identite:' + IDENTITE
+					+ '/trajet:' + trajet;
+			$.ajax({
+				url : '/sbmajaxeleve/blockaffectations/' + args1,
+				type : 'GET',
+				dataType : 'html',
+				success : function(data) {
+					var myid = '#block-affectationsr' + trajet;
+					$(myid).empty();
+					$(myid).append(data);
+					$('html', 'body').css('cursor', 'auto');
+				},
+				error : function(xhr, ajaxOptions, thrownError) {
+					alert(xhr.status + " " + thrownError);
+					$('html', 'body').css('cursor', 'auto');
+				}
+			});
+			var args2 = 'eleveId:' + ELEVE_ID + '/trajet:' + trajet;
+			var args3 = "input[name=accordR" + trajet + "][type=hidden]";
+			$.ajax({
+				url : '/sbmajaxeleve/enableaccordbutton/' + args2,
+				dataType : 'json',
+				success : function(dataJson) {
+					var myid = "#eleve-accordR" + trajet;
+					if (dataJson.enable) {
+						$(myid).removeAttr('disabled');
+						$(args3).val(0);
+					} else {
+						$(myid).attr('disabled', 'disabled');
+						$(args3).val(1);
+					}
+				},
+				error : function(xhr, ajaxOptions, throxnError) {
+					alert(xhr.status + " " + thrownError);
+				}
+			});
+		},
+		"majPaiement": function(gratuit) {
+			var libelle;
+			switch (gratuit) {
+			case '0':
+				libelle = 'Famille';
+				break;
+			case '1':
+				libelle = 'Gratuit';
+				break;
+			case '2':
+				libelle = 'Organisme';
+				break;
+			}
+			$("#inner-paiement").empty();
+			$("#inner-paiement").append(libelle);
+		}
+
+	}
+})();
