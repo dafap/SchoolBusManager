7bb1ea2f4d45ba3198b37621afe23ed5620e378d
dafap <dafap@free.fr>
2016-08-21 23:04:21 +0200

    Test unitaire pour SbmAjax\AbstractActionController.

---
+++ tests/ModulesTests/SbmAjaxTest/Controller/AbstractActionControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Test de la classe SbmAjax\AbstractActionController
+ * 
+ * @project sbm
+ * @package ModulesTests/SbmAjaxTest/Controller
+ * @filesource AbstractActionControllerTest.php
+ * @encodage UTF-8
+ * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
+ * @date 21 août 2016
+ * @version 2016-2.2.0
+ */
+namespace ModulesTests\SbmAjaxTest\Controller;
+
+use PHPUnit\Framework\TestCase;
+use Zend\Http\Request;
+use Zend\Http\Response;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+use SbmAjax\Controller\AbstractActionController as SampleController;
+
+class AbstractActionControllerTest extends TestCase
+{
+
+    public function setUp()
+    {
+        $this->request = new Request();
+        $event = new MvcEvent();
+        $event->setRequest($this->request);
+        $event->setResponse(new Response());
+        $event->setRouteMatch(new RouteMatch([
+            'value' => 'rm:1234',
+            'other' => '1234:rm',
+            'args' => 'rm:1234/kl:azerty'
+        ]));
+        $this->controller = new SampleController();
+        $this->controller->setEvent($event);
+        // $this->plugin = $this->controller->plugin('params');
+    }
+    
+    /**
+     * Test du comportement hérité du plugin Params
+     * 
+     */
+    public function testFromRouteIsDefault()
+    {
+        $value = $this->controller->params('value');
+        $this->assertEquals($value, 'rm:1234');
+    }
+    public function testFromRouteReturnsDefaultIfSet()
+    {
+        $value = $this->controller->params('foo', 'bar');
+        $this->assertEquals($value, 'bar');
+    }
+    public function testFromRouteReturnsExpectedValue()
+    {
+        $value = $this->controller->params('value');
+        $this->assertEquals($value, 'rm:1234');
+    }
+    /**
+     * Nouveau comportement pour args
+     */
+    public function testFromRouteArgs()
+    {
+        $expected = ['rm' => 1234, 'kl' => 'azerty'];
+        $args = $this->controller->params('args');
+        $this->assertEquals($expected, $args);
+    }
+    public function testFromRouteRmInArgs()
+    {
+        $expected = 1234;
+        $args = $this->controller->params('rm');
+        $this->assertEquals($expected, $args);
+    }
+}
