c9af7265819c399fe3b83079da78cf130de1be82
dafap <dafap@free.fr>
2016-10-10 12:38:52 +0200

    Ajout de la gestion des exceptions dans les controllers

---
+++ module/SbmAjax/src/SbmAjax/Controller/Exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Exception lancée par les classes Controller
+ * 
+ * @project sbm
+ * @package SbmAjax/Controller
+ * @filesource Exception.php
+ * @encodage UTF-8
+ * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
+ * @date 4 sept. 2016
+ * @version 2016-2.2.0
+ */
+namespace SbmAjax\Controller;
+
+class Exception extends \Exception
+{
+}
