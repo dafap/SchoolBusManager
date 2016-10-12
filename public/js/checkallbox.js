4c370afa24216276669c1a0cf7af34412b16bef0
dafap <dafap@free.fr>
2016-10-12 15:58:14 +0200

    Amélioration de la gestion des multicheckbox pour permettre de choisir un multicheckbox quand il y en a plusieurs dans un formulaire.

--- public/js/checkallbox.js
+++ public/js/checkallbox.js
@@ -2,14 +2,13 @@
  * Script qui gère une case à cocher "Tout cocher / Tout décocher" pour un
  * multicheckbox.
  * 
- * On lance la mise en place de la case par : multicheckbox_actions.init(id_form, id_div)
+ * On lance la mise en place de la case par : multicheckbox_actions.init(id_div)
  * où
- *   id_form est l'id du formulaire contenant le multicheckbox</li>
  *   id_div est l'id de la div contenant le multicheckbox. Elle est structurée comme suit : 
  *    <div id="...">
  *        <fieldset>
  *            <legend>...</legend>
- *            <label>...</label> 
+ *            <label>...<input type="checkbox" class="sbm-multicheckbox"...></label> 
  *            ... 
  *        </fieldset>
  *    </div>
@@ -23,13 +22,12 @@
  */
 var multicheckbox_actions = (function() {
 	var checkboxes_sel = "input.sbm-multicheckbox:enabled";
-	var id_container;
 	var id_wrapper;
 	var checkboxes_changed = function() {
-		var $form = $("#" + id_container);
-		var total_boxes = $form.find(checkboxes_sel).length;
-		var checked_boxes = $form.find(checkboxes_sel + ":checked").length;
-		var $checkall = $form.find("input.checkall_box");
+		var $container = $("#" + id_wrapper);
+		var total_boxes = $container.find(checkboxes_sel).length;
+		var checked_boxes = $container.find(checkboxes_sel + ":checked").length;
+		var $checkall = $container.find("input.checkall_box");
 		if (total_boxes == checked_boxes) {
 			$checkall.prop({
 				checked : true,
@@ -52,7 +50,7 @@ var multicheckbox_actions = (function() {
         $(document).on("change", checkboxes_sel, checkboxes_changed);
 		$(document).on("change", "input.checkall_box", function() {
 			var is_checked = $(this).is(":checked");
-			$(this.form).find(checkboxes_sel).prop("checked", is_checked);
+			$("#"+id_wrapper).find(checkboxes_sel).prop("checked", is_checked);
 		});
 
 		var legend = $("#" + id_wrapper + " fieldset legend");
@@ -63,8 +61,7 @@ var multicheckbox_actions = (function() {
 		checkboxes_changed();
 	});
 	return {
-		"init" : function(id_form, id_div) {
-			id_container = id_form;
+		"init" : function(id_div) {
 			id_wrapper = id_div;
 		}
 	}
