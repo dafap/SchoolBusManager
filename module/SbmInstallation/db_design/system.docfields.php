6500afeeb23dbfff34bda3462f2d4f65634a5c63
dafap <dafap@free.fr>
2019-01-26 11:16:27 +0100

    Modification de la définition de la table system docfields pour gérer les paramètres d'une photo.

--- module/SbmInstallation/db_design/system.docfields.php
+++ module/SbmInstallation/db_design/system.docfields.php
@@ -8,8 +8,8 @@
  * @filesource system.docfields.php
  * @encodage UTF-8
  * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
- * @date 7 avr. 2018
- * @version 2018-2.4.0
+ * @date 25 jan. 2019
+ * @version 2019-2.4.6
  */
 return [
     'name' => 'docfields',
@@ -29,7 +29,7 @@ return [
             'fieldname_stretch' => 'tinyint(1) UNSIGNED NOT NULL DEFAULT "0"', // si fieldname_width alors de 0 à 4
             'fieldname_completion' => 'tinyint(3) NOT NULL DEFAULT "0"', // si fieldname_width alors complétion à gauche par des espaces ou zéros. Indique le nombre de chiffres à obtenir. 0 <=> pas de complétion
             'fieldname_precision' => 'tinyint(3) NOT NULL DEFAULT "-1"', // précision pour les colonnes numériques. -1 pour ne pas appliquer la précision
-            'is_date' => 'tinyint(1) UNSIGNED NOT NULL DEFAULT "0"', // 0 ou 1
+            'nature' => 'tinyint(1) UNSIGNED NOT NULL DEFAULT "0"', // 0 ou 1 pour date ou 2 pour photo
             'format' => 'varchar(255)', // format de date : on vérifie s'il y a un h (date J/M/A H:m:s) ou non (date J/M/A) ou format de sprintf si non date
             'label' => 'text NULL', // le texte à imprimer avant la valeur du data
             'label_space' => 'float NOT NULL DEFAULT "3"', // espacement entre le label et la donnée (en unité du document, en général le mm)
@@ -37,7 +37,14 @@ return [
             'label_align' => 'varchar(8) NOT NULL DEFAULT "standard"', // si label_width alors L, C, R, J
             'label_stretch' => 'tinyint(1) UNSIGNED NOT NULL DEFAULT "0"', // si label_width alors de 0 à 4
             'style' => 'varchar(6) NOT NULL DEFAULT "main"', // main, data, titre1, titre2, titre3 ou titre4
-            'height' => 'float NOT NULL DEFAULT "7"'
+            'height' => 'float NOT NULL DEFAULT "7"',
+            'photo_x' => 'int(11) NULL DEFAULT NULL',
+            'photo_y' => 'int(11) NULL DEFAULT NULL',
+            'photo_w' => 'int(11) NOT NULL DEFAULT "0"',
+            'photo_h' => 'int(11) NOT NULL DEFAULT "0"',
+            'photo_type' => 'varchar(4) NOT NULL DEFAULT "JPEG"',
+            'photo_align' => 'char(1) NULL DEFAULT NULL',
+            'photo_resize' => 'tinyint NOT NULL DEFAULT 1'
         ], // hauteur des cellules d'écriture du field et du label
         'primary_key' => [
             'docfieldId'
