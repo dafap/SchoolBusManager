4336eb88d2cccdf9f0af05b99a00ecefee33540d
dafap <dafap@free.fr>
2019-01-30 09:30:26 +0100

    Ajustement technique du repository
    Permet d'éliminer les alertes liées à Git.

--- vendor/bin/templatemap_generator.php
+++ vendor/bin/templatemap_generator.php
@@ -1,5 +1,10 @@
-#!/usr/bin/env sh dir=$(cd "${0%[/\\]*}" > /dev/null; cd
-"../zendframework/zend-view/bin" && pwd) if [ -d /proc/cygdrive ] && [[
-$(which php) == $(readlink -n /proc/cygdrive)/* ]]; then # We are in
-Cgywin using Windows php, so the path must be translated dir=$(cygpath
--m "$dir"); fi "${dir}/templatemap_generator.php" "$@"
+#!/usr/bin/env sh
+
+dir=$(cd "${0%[/\\]*}" > /dev/null; cd "../zendframework/zend-view/bin" && pwd)
+
+if [ -d /proc/cygdrive ] && [[ $(which php) == $(readlink -n /proc/cygdrive)/* ]]; then
+   # We are in Cgywin using Windows php, so the path must be translated
+   dir=$(cygpath -m "$dir");
+fi
+
+"${dir}/templatemap_generator.php" "$@"
