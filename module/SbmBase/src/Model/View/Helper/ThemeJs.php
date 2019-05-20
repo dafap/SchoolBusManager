b00d51622888e93ea52e83ba8676c0f70e0fb440
dafap <dafap@free.fr>
2019-05-20 23:37:42 +0200

    Amélioration permettant de préciser la position 'append' ou 'prepend' ('append' par défaut) et la pile 'head' ou 'inline' ('head' par défaut).

--- module/SbmBase/src/Model/View/Helper/ThemeJs.php
+++ module/SbmBase/src/Model/View/Helper/ThemeJs.php
@@ -8,7 +8,7 @@
  * @filesource ThemeJs.php
  * @encodage UTF-8
  * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
- * @date 29 avr. 2019
+ * @date 20 mai 2019
  * @version 2019-2.5.0
  */
 namespace SbmBase\Model\View\Helper;
@@ -28,23 +28,35 @@ class ThemeJs extends AbstractHelper
 
     public function __construct($theme_name)
     {
-        $this->theme_name = strtolower($theme_name);;
+        $this->theme_name = strtolower($theme_name);
+        ;
     }
 
     /**
-     * Met le fichier indiqué dans la pile headScript
+     * Met le fichier indiqué dans la pile headScript ou du inlineScript
      *
      * @param string $phtml_path
-     *            chemin après le dossier theme
+     *            chemin après le dossier theme, peut être ''
      * @param string $js_file
-     *            nom du fichier
+     *            nom du fichier, doit être non ''
+     * @param string $position
+     *            'append' ou 'prepend'
+     * @param string $pile
+     *            'head' ou 'inline'
      */
-    public function __invoke($phtml_path, $js_file)
+    public function __invoke(string $phtml_path, string $js_file,
+        string $position = 'append', string $pile = 'head')
     {
+        if ($position != 'prepend') {
+            $position = 'append';
+        }
+        if ($pile != 'inline') {
+            $pile = 'head';
+        }
         $basepath = $this->getView()->plugin('basepath');
-        $headScript = $this->getView()->plugin('headscript');
+        $headScript = $this->getView()->plugin($pile . 'script');
         $path = sprintf('/js/%s/%s', $this->theme_name, $phtml_path);
         $js = StdLib::concatPath($path, $js_file);
-        $headScript->appendFile($basepath($js));
+        $headScript->{$position . 'File'}($basepath($js));
     }
 }
