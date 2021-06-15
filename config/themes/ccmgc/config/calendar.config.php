3919573ad8b898e243ef2354c777dc7cdea44d68
Pomirol Alain <dafap@free.fr>
2021-06-15 19:41:58 +0200

    Modification du format du calendar
    Suppression de la saisie des vacances et des permanences.

--- config/themes/ccmgc/config/calendar.config.php
+++ config/themes/ccmgc/config/calendar.config.php
@@ -8,13 +8,18 @@
  * Pour les permanences, indiquer le code INSEE de la commune dans le champ 'libelle'.
  * La méthode SbmGestion\Controller\AnneeScolaireController::newAction() fera le reste.
  *
+ * Remarque: à partir de la version 2021-2.5.11 on réduit la liste aux natures AS et INS
+ * car il n'y a plus de permanence pour la remise des cartes.
+ * Si nécessaire, voir les versions précédentes sur GITHUB pour revenir à la gestion des
+ * dates de permanences.
+ *
  * @project sbm
  * @package SbmGestion/Model
  * @filesource Modele.inc.php
  * @encodage UTF-8
  * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
- * @date 3 mai 2019
- * @version 2019-2.5.0
+ * @date 15 juin 2021
+ * @version 2021-2.5.11
  */
 return [
     [
@@ -31,158 +36,6 @@ return [
         'rang' => 1,
         'libelle' => 'Période d\'inscription',
         'description' => '%libelle% %as%',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 21,
-        'nature' => 'VACA',
-        'rang' => 1,
-        'libelle' => 'Vacances de Toussaint',
-        'description' => 'Vacances de Toussaint %as%',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 22,
-        'nature' => 'VACA',
-        'rang' => 2,
-        'libelle' => 'Vacances de Noël',
-        'description' => 'Vacances de Noël %as%',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 23,
-        'nature' => 'VACA',
-        'rang' => 3,
-        'libelle' => 'Vacances d\'hiver',
-        'description' => 'Vacances d\'hiver %as%',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 24,
-        'nature' => 'VACA',
-        'rang' => 4,
-        'libelle' => 'Vacances de printemps',
-        'description' => 'Vacances de printemps %as%',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 101,
-        'nature' => 'PERM',
-        'rang' => 1,
-        'libelle' => '12002',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 102,
-        'nature' => 'PERM',
-        'rang' => 2,
-        'libelle' => '12070',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 103,
-        'nature' => 'PERM',
-        'rang' => 3,
-        'libelle' => '12072',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 104,
-        'nature' => 'PERM',
-        'rang' => 4,
-        'libelle' => '12084',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 105,
-        'nature' => 'PERM',
-        'rang' => 5,
-        'libelle' => '12086',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 106,
-        'nature' => 'PERM',
-        'rang' => 6,
-        'libelle' => '12145',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 107,
-        'nature' => 'PERM',
-        'rang' => 7,
-        'libelle' => '12160',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 108,
-        'nature' => 'PERM',
-        'rang' => 8,
-        'libelle' => '12178',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 109,
-        'nature' => 'PERM',
-        'rang' => 9,
-        'libelle' => '12180',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 110,
-        'nature' => 'PERM',
-        'rang' => 10,
-        'libelle' => '12200',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 111,
-        'nature' => 'PERM',
-        'rang' => 11,
-        'libelle' => '12204',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 112,
-        'nature' => 'PERM',
-        'rang' => 12,
-        'libelle' => '48131',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 113,
-        'nature' => 'PERM',
-        'rang' => 13,
-        'libelle' => '12211',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 114,
-        'nature' => 'PERM',
-        'rang' => 14,
-        'libelle' => '12225',
-        'description' => '',
-        'exercice' => 0
-    ],
-    [
-        'ordinal' => 115,
-        'nature' => 'PERM',
-        'rang' => 15,
-        'libelle' => '12293',
-        'description' => '',
         'exercice' => 0
     ]
 ];
--- module/SbmGestion/src/Model/Modele.inc.php
+++ module/SbmGestion/src/Model/Modele.inc.php
@@ -7,8 +7,8 @@
  * @filesource Modele.inc.php
  * @encodage UTF-8
  * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
- * @date 27 juin 2017
- * @version 2017-2.3.4
+ * @date 15 juin 2021
+ * @version 2021-2.5.11
  */
 return [
     'annee-scolaire' => [
@@ -28,87 +28,7 @@ return [
                 'libelle' => 'Période d\'inscription',
                 'description' => '%libelle% %as%',
                 'exercice' => 0
-            ],
-            [
-                'ordinal' => 3,
-                'nature' => 'PERM',
-                'rang' => 1,
-                'libelle' => 'LANGON',
-                'description' => 'Langon le',
-                'exercice' => 0
-            ],
-            [
-                'ordinal' => 4,
-                'nature' => 'PERM',
-                'rang' => 2,
-                'libelle' => 'CASTETS-EN-DORTHE',
-                'description' => 'Castets-en-Dorthe le',
-                'exercice' => 0
-            ],
-            [
-                'ordinal' => 5,
-                'nature' => 'PERM',
-                'rang' => 3,
-                'libelle' => 'SAINT-PARDON-DE-CONQUES',
-                'description' => 'Castets-en-Dorthe le',
-                'exercice' => 0
-            ],
-            [
-                'ordinal' => 6,
-                'nature' => 'PERM',
-                'rang' => 4,
-                'libelle' => 'BIEUJAC',
-                'description' => 'Bieujac le',
-                'exercice' => 0
-            ],
-            [
-                'ordinal' => 7,
-                'nature' => 'PERM',
-                'rang' => 5,
-                'libelle' => 'FARGUES',
-                'description' => 'Fargues le',
-                'exercice' => 0
-            ],
-            [
-                'ordinal' => 8,
-                'nature' => 'PERM',
-                'rang' => 6,
-                'libelle' => 'BOMMES',
-                'description' => 'Bommes le',
-                'exercice' => 0
-            ],
-            [
-                'ordinal' => 9,
-                'nature' => 'VACA',
-                'rang' => 1,
-                'libelle' => 'Vacances de Toussaint',
-                'description' => 'Vacances de Toussaint %as%',
-                'exercice' => 0
-            ],
-            [
-                'ordinal' => 10,
-                'nature' => 'VACA',
-                'rang' => 2,
-                'libelle' => 'Vacances de Noël',
-                'description' => 'Vacances de Noël %as%',
-                'exercice' => 0
-            ],
-            [
-                'ordinal' => 11,
-                'nature' => 'VACA',
-                'rang' => 3,
-                'libelle' => 'Vacances d\'hiver',
-                'description' => 'Vacances d\'hiver %as%',
-                'exercice' => 0
-            ],
-            [
-                'ordinal' => 12,
-                'nature' => 'VACA',
-                'rang' => 4,
-                'libelle' => 'Vacances de printemps',
-                'description' => 'Vacances de printemps %as%',
-                'exercice' => 0
             ]
         ]
     ]
-]; 
+];
