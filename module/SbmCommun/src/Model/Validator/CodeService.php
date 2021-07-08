2993964cf56c9efe928cca56e63437427d2a2574
Pomirol Alain <dafap@free.fr>
2021-07-08 18:32:40 +0200

    Modification du validateur pour un codeService

--- module/SbmCommun/src/Model/Validator/CodeService.php
+++ module/SbmCommun/src/Model/Validator/CodeService.php
@@ -9,8 +9,8 @@
  * @filesource CodeService.php
  * @encodage UTF-8
  * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
- * @date 17 avr. 2019
- * @version 2019-2.5.0
+ * @date 8 juil. 2021
+ * @version 2021-2.5.13
  */
 namespace SbmCommun\Model\Validator;
 
@@ -25,7 +25,7 @@ use Zend\Validator\AbstractValidator;
 class CodeService extends AbstractValidator
 {
 
-    const PATTERN = '/^M[4][0-9]{2}[A-Z](?:-V[1-9])?$|^MGC[0-9]{2}(?:-[NV][1-9])?$/';
+    const PATTERN = '/^M[4][0-9]{2}[A-Z](?:-V[1-9])?$|^MGC[0-9]{2,3}[A-Z]?(?:-[NV][1-9])?$/';
 
     const ERROR = 'codeService';
 
