98dd646eb7abac6a050c2d51a58439bf83a77a08
Pomirol Alain <dafap@free.fr>
2022-11-11 18:15:07 +0100

    Correction de structure suite aux changement de serveur chez Ex2Hosting
    La base de données est désormais sous MariaDb
    La colonne id_int NE DOIS PAS avoir la propriété NOT NULL

--- module/SbmInstallation/db_design/system.history.php
+++ module/SbmInstallation/db_design/system.history.php
@@ -9,8 +9,8 @@
  * @filesource system.history.php
  * @encodage UTF-8
  * @author DAFAP Informatique - Alain Pomirol (dafap@free.fr)
- * @date 21 déc. 2018
- * @version 2019-2.5.4
+ * @date 11 nov. 2022
+ * @version 2019-2.5.7
  */
 return [
     'name' => 'history',
@@ -23,10 +23,10 @@ return [
             'table_name' => 'varchar(32) DEFAULT NULL',
             'action' => 'char(6) NOT NULL',
             'id_name' => 'varchar(64) DEFAULT NULL',
-            'id_int' => 'int(11) NOT NULL DEFAULT 0',
+            'id_int' => 'int(11) DEFAULT 0',  // suppression de la propriété NOT NULL
             'id_txt' => 'varchar(25) DEFAULT NULL', // calculé pour affectations
             'dt' => 'datetime NOT NULL',
-            'log' => 'text'
+            'log' => 'text DEFAULT NULL'
         ],
         'keys' => [
             'HISTORY_Table' => [
